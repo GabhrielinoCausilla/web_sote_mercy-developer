4a3a66b7ad8343342a2438f070d61385eccbff9f
Mr-Ottoman <ariel.perez.caro@cftmail.cl>
2024-10-24 12:11:14 -0300

    Evaluacion edgardo

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { UbicanosComponent } from './ubicanos/ubicanos.component';
+import { ContactanosComponent } from './contactanos/contactanos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    UbicanosComponent,
+    ContactanosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
